05e3e34e3d0f0cad4903ac32ec65a3dfea7f17ed
Gaurav Paudel <gaurav.paudel@wesionary.team>
2023-02-12 11:50:03 +0545

    :sparkles: feat: added checking if subset utility function :

--- utils/exists_in_slice.go
+++ utils/exists_in_slice.go
@@ -8,3 +8,12 @@ func Exists(s string, slice []string) bool {
 	}
 	return false
 }
+
+func SubSet(s1, s2 []string) bool {
+	for _, s := range s1 {
+		if !Exists(s, s2) {
+			return false
+		}
+	}
+	return true
+}
